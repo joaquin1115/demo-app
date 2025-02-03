1648443bdc9551eb0c1c81b66ae3fab98f5a131c
José Joaquín Alvarado Usucachi <joaquin14142@gmail.com>
2025-02-02 22:35:38 -0500

    edit backend

--- infrastructure/app-infrastructure/provider.tf
+++ infrastructure/app-infrastructure/provider.tf
@@ -1,13 +1,3 @@
 provider "aws" {
   region = var.aws_region
 }
-
-terraform {
-  backend "s3" {
-    bucket         = "your-company-terraform-state"
-    key            = "app/terraform.tfstate"
-    region         = var.aws_region
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
--- infrastructure/state-backend/variables.tf
+++ infrastructure/state-backend/variables.tf
@@ -6,11 +6,9 @@ variable "aws_region" {
 variable "state_bucket_name" {
   description = "Name of the S3 bucket for Terraform state"
   type        = string
-  default     = "your-terraform-state-bucket"
 }
 
 variable "dynamodb_table_name" {
   description = "Name of the DynamoDB table for state locking"
   type        = string
-  default     = "terraform-state-lock"
 }
