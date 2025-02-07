6b8fe18753165e4b638e65240b16efe124843165
José Joaquín Alvarado Usucachi <joaquin14142@gmail.com>
2025-02-06 20:45:00 -0500

    Update data.tf

--- infrastructure/app-infrastructure/data.tf
+++ infrastructure/app-infrastructure/data.tf
@@ -7,5 +7,5 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy" "AmazonECSTaskExecutionRolePolicy" {
-  arn = "arn:aws:iam::aws:policy/AmazonECSTaskExecutionRolePolicy"
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
