4ca93672377cc2c21ff112a8eff2f40fa6f6cf1f
José Joaquín Alvarado Usucachi <joaquin14142@gmail.com>
2025-02-08 19:59:56 -0500

    Update outputs.tf

--- infrastructure/app-infrastructure/outputs.tf
+++ infrastructure/app-infrastructure/outputs.tf
@@ -45,8 +45,8 @@ output "cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.app_distribution.id
 }
 
-output "ecr_repository_url" {
-  value = aws_ecr_repository.my_ecr_repo.repository_url
+output "ecr_repository_name" {
+  value = aws_ecr_repository.my_ecr_repo.name
 }
 
 output "user_pool_id" {
